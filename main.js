836dd5408eb3bb426c2cb0d7af195626cf3e7ecb
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-02-16 11:06:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,39 +3,57 @@ const materials = ['Hylian Rice', 'Big Hearty Truffle', 'Tabantha Wheat', 'Raw P
 
 const meals = []
 
-function cook(firstIngredient, secondIngredient, thirdIngredient) {
-    for (i = 0; i < materials.length; i++) {
-    if (firstIngredient[i] === 'Hylian Rice' && secondIngredient[i] === 'Big Hearty Truffle') {
-        meals.push('Mushroom Rice Balls')       
-    } if (firstIngredient[i] === 'Hateno Cheese' && secondIngredient[i] === 'Bird Egg') {
-        meals.push('Cheezy Omelette')
-    } if (firstIngredient[i] === 'Tabantha Wheat' && secondIngredient[i] === 'Hateno Cheese') {
-        meals.push('Cheezy Hylian Pizza')
-    } if (firstIngredient[i] === 'Hylian Rice' && secondIngredient[i] === 'Raw Prime Meat') {
-        meals.push('Prime Meat and Rice Bowl')
-    } if (firstIngredient[i] === 'Bird Egg' && secondIngredient[i] === 'Fresh Milk' && thirdIngredient[i] === 'Cane Sugar') {
-        meals.push('Egg Pudding')
-    }        
-}
-}
-cook()
-
 materials.push('Fresh Milk', 'Cane Sugar', 'Goat Butter', 'Raw Bird Thigh')
-//console.log(materials)
+console.log(materials)
+/*
+function cook() {
+    for (i = 0; i < materials.length; i++) {
+        if (materials[i] === 'Hylian Rice' && 'Big Hearty Truffle') {
+            meals.push('Mushroom Rice Balls')         
+    }   if (materials[i] === 'Hateno Cheese' && 'Bird Egg') {
+            meals.push('Cheezy Omelette')
+    }   if (materials[i] === 'Tabantha Wheat' && 'Hateno Cheese') {
+            meals.push('Cheezy Hylian Pizza')
+    }   if (materials[i] === 'Hylian Rice' && 'Raw Prime Meat') { 
+            meals.push('Prime Meat and Rice Bowl')
+    }   if (materials[i] === 'Bird Egg' && 'Fresh Milk' && 'Cane Sugar') {
+            meals.push('Egg Pudding')
+    }   if (materials[i] === 'Tabantha Wheat' && 'Cane Sugar' && 'Goat Butter' && 'Bird Egg') {
+            meals.push('Egg Tart')  
+    }   if (materials[i] === 'Hylian Rice' && 'Raw Prime Meat' && 'Bird Egg') {
+            meals.push('Chicken Egg Fried Rice')
+    }   if (materials[i] === 'Hylian Rice' && 'Goat Butter' && 'Bird Egg' && 'Raw Bird Thigh') {
+            meals.push('Poultry Pilaf')
+    }
+}
+}
+*/
 
-//function cook() {
-//    for (i = 0; i < materials.length; i++) {
-//        if (materials[i] === 'Hylian Rice' && materials[i + 1] === 'Big Hearty Truffle') {
-//            meals.push('Mushroom Rice Balls')         
-//    }   else if (materials[i] === 'Hateno Cheese' && materials[i + 1] === 'Bird Egg') {
-//            meals.push('Cheezy Omelette')
-//    }   else if (materials[i] === 'Tabantha Wheat' && materials[i + 1] === 'Hateno Cheese') {
-//            meals.push('Cheezy Hylian Pizza')
-//        }
-//}
-//}
+const cook = (ingredients) => {
+    for (i = 0; i < ingredients.length; i++) {
+        if (ingredients[i] === 'Hylian Rice' && 'Big Hearty Truffle') {
+            meals.push('Mushroom Rice Balls')         
+    }   if (ingredients[i] === 'Hateno Cheese' && 'Bird Egg') {
+            meals.push('Cheezy Omelette')
+    }   if (ingredients[i] === 'Tabantha Wheat' && 'Hateno Cheese') {
+            meals.push('Cheezy Hylian Pizza')
+    }   if (ingredients[i] === 'Hylian Rice' && 'Raw Prime Meat') { 
+            meals.push('Prime Meat and Rice Bowl')
+    }   if (ingredients[i] === 'Bird Egg' && 'Fresh Milk' && 'Cane Sugar') {
+            meals.push('Egg Pudding')
+    }   if (ingredients[i] === 'Tabantha Wheat' && 'Cane Sugar' && 'Goat Butter' && 'Bird Egg') {
+            meals.push('Egg Tart')  
+    }   if (ingredients[i] === 'Hylian Rice' && 'Raw Prime Meat' && 'Bird Egg') {
+            meals.push('Chicken Egg Fried Rice')
+    }   if (ingredients[i] === 'Hylian Rice' && 'Goat Butter' && 'Bird Egg' && 'Raw Bird Thigh') {
+            meals.push('Poultry Pilaf')
+    } 
+}
+}
+const newRecipes = cook(materials)
 
 
+//cook()
 
 
 
@@ -45,11 +63,6 @@ console.log('---------------')
 console.log(meals)
 
 
-//TO DO:
-
-
-//4. Update existing function to include more ingredient parameters
-//5. Add second set of new recipes to meals array
 
 
 //QUESTIONS:
